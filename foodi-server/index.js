aeb818a29ad8f59e1a9fa95cd615725132bcafa1
ujjwalkran <karanujjwal2401@gmail.com>
2024-03-26 22:20:39 +0530

    First commit

---
+++ foodi-server/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+const jwt = require('jsonwebtoken');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@foodi-app.si7kevz.mongodb.net/?retryWrites=true&w=majority&appName=foodi-app`
+  )
+  .then(console.log("Mongodb connected successfully!"))
+  .catch((error) => console.log("Error connecting to MongoDB: " + error));
+
+// jwt authentication
+
+// jwt related api
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  // console.log(user)
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  });
+  res.send({ token });
+});
+
+// import routes
+const menuRoutes = require("./api/routes/menuRoutes");
+const cartsRoutes = require("./api/routes/cartRoutes");
+const usersRoutes = require("./api/routes/userRoutes");
+const paymentRoutes = require("./api/routes/paymentRoutes");
+const adminStats =  require('./api/routes/adminStats');
+ const orderStats = require('./api/routes/orderStats')
+app.use("/menu", menuRoutes);
+app.use("/carts", cartsRoutes);
+app.use("/users", usersRoutes);
+app.use("/payments", paymentRoutes);
+app.use('/admin-stats', adminStats);
+app.use('/order-stats', orderStats);
+
+
+// payment methods routes
+const verifyToken = require('./api/middlewares/verifyToken')
+
+app.post("/create-payment-intent",verifyToken, async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price*100);
+  // console.log(amount);
+  
+
+  // Create a PaymentIntent 
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "inr",
+    payment_method_types: ["card"],
+    description:"payment initiated"
+  });
+
+  
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Foodi Server is Running!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
